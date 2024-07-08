35cdc6439e2b651f0097af582d043a3857cb3c0b
adalysg <adalys39@gmail.com>
2024-07-08 18:16:02 -0400

    updated user route/model to accept permissions

--- MERN/backend_server/models/User.js
+++ MERN/backend_server/models/User.js
@@ -5,6 +5,7 @@ const UserSchema = new mongoose.Schema({
   phone: String,
   email: { type: String, unique: true },
   password: String,
+  permissions: Enumerator,
   createdAt: { type: Date, default: Date.now }
 });
 
