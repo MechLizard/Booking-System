de9b78fd44de81ab01d71a9bb7d18d052917985b
qwasmos <qwasmos0102@gmail.com>
2024-07-09 15:29:26 -0400

    These are my local changes

--- MERN/backend_server/models/User.js
+++ MERN/backend_server/models/User.js
@@ -1,11 +1,20 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
+const UserCustomer = new mongoose.Schema({
   name: String,
   phone: String,
   email: { type: String, unique: true },
   password: String,
   createdAt: { type: Date, default: Date.now }
+});
+
+const UserBusiness = new mongoose.Schema({
+  name: String,
+  phone: String,
+  email: {type: string, unique: true},
+  password: String,
+
+  createdAt: {type: Data, default: Date.now}
 });
 
 module.exports = mongoose.model('User', UserSchema);
