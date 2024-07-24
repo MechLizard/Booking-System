2180f9de6565dba1bc0c4ad0d31f01c5714be6dc
Izzy Jones <jones.r1@ufl.edu>
2024-07-24 10:42:13 -0400

    Added compile scripts for local testing

---
+++ docker_compile_script.bat
@@ -0,0 +1,34 @@
+@echo off
+
+REM stop any previous instances
+echo "Stopping previous the-feed containers..."
+docker stop the-feed
+
+REM Remove old container to avoid conflicts
+docker rm the-feed
+
+REM Pull the docker image
+echo "Downloading docker image..."
+docker pull mechlizard/the-feed-web:latest
+
+REM Create a container from the image
+echo "Creating container..."
+docker create -p 3000:3000 --name the-feed mechlizard/the-feed-web:latest
+
+REM Copy files from the current directory to the container
+echo "Copying project files..."
+docker cp ./MERN the-feed:/home/circleci/project/MERN
+docker cp ./public the-feed:/home/circleci/project/public
+docker cp ./src the-feed:/home/circleci/project/src
+docker cp ./test the-feed:/home/circleci/project/test
+docker cp ./package.json:/home/circleci/project
+docker cp ./package-lock.json:/home/circleci/project
+
+REM start the container
+echo "Starting the container..."
+docker start the-feed
+
+REM Run the container
+docker logs -f the-feed
+
+@echo on
